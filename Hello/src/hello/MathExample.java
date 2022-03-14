ce4f80c8782f282dd51dc83fbf0bb333e8e26aa3
deuxist <deuxist@DESKTOP-R3TDTRB>
2022-03-14 22:59:26 +0900

    homework

---
+++ Hello/src/hello/MathExample.java
@@ -0,0 +1,23 @@
+package hello;
+import java.util.Scanner;
+public class MathExample {
+
+	public static void main(String[] args) {
+	Scanner scn = new Scanner(System.in);
+	System.out.println("첫번째 수를 입력하세요");
+	double a = scn.nextDouble();
+	System.out.println("두번째 수를 입력하세요");
+	double b = scn.nextDouble();
+	double c= a+b;
+	double d= a-b;
+	double e= a*b;		
+	double f= a/b;
+	System.out.println("두수의 합은 "+c);
+	System.out.println("두수의 차는 "+d);
+	System.out.println("두수의 곱은 "+e);
+	System.out.println("두수의 나누기는 "+f);
+	scn.close();
+	
+	}
+
+}
