0857416b2c0d4de6db208bc5caf14ff82e680324
admin <admin@YD-19>
2022-03-14 13:57:16 +0900

    4th

--- Hello/src/hello/Hello.java
+++ Hello/src/hello/Hello.java
@@ -3,9 +3,9 @@ package hello;
 public class Hello {
 
 	public static void main(String[] args) {
-	  System.out.println("안녕하세요");	// TODO Auto-generated method stub
-      System.out.println("저는 권경완입니다");
-      System.out.println("저는 권경완입니다");
+		//첫번째 프로그램입니다. 
+		System.out.println("안녕하세요"); // TODO Auto-generated method stub
+		System.out.println("저는 권경완입니다");
+		System.out.println("저는 권경완입니다");
 	}
-
 }
