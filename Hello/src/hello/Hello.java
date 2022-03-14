527831d3a9999ff88a69f85cc4a42dbacc3a4b9a
admin <admin@YD-19>
2022-03-14 11:10:51 +0900

    second commit

--- Hello/src/hello/Hello.java
+++ Hello/src/hello/Hello.java
@@ -4,7 +4,7 @@ public class Hello {
 
 	public static void main(String[] args) {
 	  System.out.println("안녕하세요");	// TODO Auto-generated method stub
-
+      System.out.println("저는 권경완입니다");
 	}
 
 }
