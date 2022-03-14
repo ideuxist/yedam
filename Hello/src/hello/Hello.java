d3e850a2f59535b02e5b317419992ae609e1b349
admin <admin@YD-19>
2022-03-14 11:21:05 +0900

    3th

--- Hello/src/hello/Hello.java
+++ Hello/src/hello/Hello.java
@@ -5,6 +5,7 @@ public class Hello {
 	public static void main(String[] args) {
 	  System.out.println("안녕하세요");	// TODO Auto-generated method stub
       System.out.println("저는 권경완입니다");
+      System.out.println("저는 권경완입니다");
 	}
 
 }
