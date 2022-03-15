21342ab06ac8bf9cb5d6f670457edae4fa8fa84b
admin <admin@YD-19>
2022-03-15 17:45:12 +0900

    0315 class

--- Hello/src/com/dev/ForExample.java
+++ Hello/src/com/dev/ForExample.java
@@ -7,7 +7,7 @@ public class ForExample {
 		int sum = 0;
 		for (int i = 1; i <= 10; i++) {
 			sum = sum + i;
-			System.out.println(sum);
+			//System.out.println(sum);
 		}
 			System.out.println(sum);
 	}
---
+++ Hello/src/com/dev/ForExample5.java
@@ -0,0 +1,18 @@
+package com.dev;
+
+import java.util.Scanner;
+
+public class ForExample5 {
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		System.out.println("두 수를 입력하세요");
+		int a = scn.nextInt();
+	}
+}
+
+		
+		
+//		System.out.println("최대공약수:"+ );
+//		System.out.println("최소공배수:"+ );
+	
+
