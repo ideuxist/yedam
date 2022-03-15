4e62b602c8e3e450b78a5d730741f5e4e714ed04
admin <admin@YD-19>
2022-03-15 17:48:24 +0900

    0315 study

---
+++ Hello/src/com/dev/ForExample4.java
@@ -0,0 +1,30 @@
+package com.dev;
+
+public class ForExample4 {
+	public static void main(String[] args) {
+		// 1~10 수중에서 짝수의 값=evenSum,
+		// 홀수의 값=oddSum
+		// 홀수의 합?? 짝수의 합 ??입니다
+//int evenSum=0;
+//int oddSum=0;
+//for (int i=0; i<=10; i++) {
+//	if (i % 2 == 0) {
+//		evenSum= evenSum + i;
+//	} else {
+//		oddSum= oddSum +i ;
+//			}
+//}
+//	System.out.println("홀수의 합은" + oddSum);
+//	System.out.println("짝수의 합은" + evenSum);
+//}
+//
+//}
+//		int num = 48;
+//		for (int i = 1; i <= 48;  i++) {
+//			if (num % i == 0) {
+//				System.out.println(i);
+//			}
+		}
+
+	}
+
