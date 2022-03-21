0f6b312f1fc2d8af1bd941ad15421e05181f0dc2
deuxist <deuxist@DESKTOP-R3TDTRB>
2022-03-21 20:21:23 +0900

    11

--- P20220318/src/com/test/Exam01.java
+++ P20220318/src/com/test/Exam01.java
@@ -2,22 +2,22 @@ package com.test;
 
 import java.util.Scanner;
 
-//문제1) 정수형 변수 num1, num2 를 선언하고 두 수 중에서 큰 수에서 작은 수를 뺀 결과를 저장하는 변수 result.
+//臾몄젣1) �젙�닔�삎 蹂��닔 num1, num2 瑜� �꽑�뼵�븯怨� �몢 �닔 以묒뿉�꽌 �겙 �닔�뿉�꽌 �옉�� �닔瑜� 類� 寃곌낵瑜� ���옣�븯�뒗 蹂��닔 result.ddd
 public class Exam01 {
 	public static void main(String[] args) {
 	
 		Scanner scn = new Scanner(System.in);
-		System.out.println(" 아무수나 입력하세요");
+		System.out.println(" �븘臾댁닔�굹 �엯�젰�븯�꽭�슂");
 		int num1 = scn.nextInt();
-		System.out.println("아무수나 입력하세요");
+		System.out.println("�븘臾댁닔�굹 �엯�젰�븯�꽭�슂");
 		int num2 = scn.nextInt();
 		
 		
 		if (num1 < num2 ) {
-			System.out.println("둘 중 더 큰수는 " + num2);
+			System.out.println("�몮 以� �뜑 �겙�닔�뒗 " + num2);
 			
 		}else { 
-		System.out.println("둘 중 더 큰수는 " +num1);
+		System.out.println("�몮 以� �뜑 �겙�닔�뒗 " +num1);
 		}
 		
 	}
