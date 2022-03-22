bc8d03bf2b37505c0627fa3412041454a5ba2689
admin <admin@YD-19>
2022-03-22 11:31:17 +0900

    12

---
+++ P20220318/src/com/edu/nested/Outer.java
@@ -0,0 +1,56 @@
+package com.edu.nested;
+
+public class Outer {
+	//
+	
+	private String field1;
+	private int field2;
+	
+	class Inner { //멤버 클래스
+		String field3;
+		int field4;
+		void method2() {
+			System.out.println("Inner.method2()");
+		}
+	}
+	interface Runnable {
+		void run();
+	}
+	class Bus implements Runnable {
+
+		@Override
+		public void run() {
+			
+			System.out.println("Bus가 달립니다");
+		}
+		
+	}
+	
+	public String getField1() {
+		return field1;
+	}
+	public void method1() {
+		System.out.println("Outer.method1()");
+		Inner inner = new Inner();
+		inner.method2();
+	}
+	public void method3() {
+		System.out.println("Outer.method3()");
+		class Local { // 로컬클래스
+			void method4() {
+				System.out.println("Local.method4()");
+			}
+			
+		}
+		Local local = new Local();
+		local.method4();
+	}
+	public void method4() {
+		Runnable runnable = new Bus(); //인터페이스의 구현객체
+		runnable.run();
+		
+		
+	}
+	
+
+}
