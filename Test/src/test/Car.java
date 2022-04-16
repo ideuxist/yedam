3a8b3c9c0a06c8854728ef9646e3faf59e051687
deuxist <deuxist@DESKTOP-R3TDTRB>
2022-04-16 22:21:14 +0900

    0416

--- Test/src/test/Car.java
+++ Test/src/test/Car.java
@@ -2,26 +2,8 @@ package test;
 
 public class Car {
 	
-	 try
-     {
-         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-         String str = "";
-
-         while((str = br.readLine()) != null)
-         {
-             System.out.println(str);
-         }
-
-     }
-     catch (Exception e)
-     {
-         e.printStackTrace();
-     }
-     
- }
-
-
-
-
 }
+
+
+
+
